ffe878c2a776be3bf02883feade6e154ef502126
Nam Ton <namtonnest@gmail.com>
2025-09-07 18:19:39 -0400

    chore: save workspace changes

--- api_disabled/createPost.js
+++
@@ -1,7 +0,0 @@
-// MOVED: api/createPost.js
-// Disabled to reduce number of serverless functions deployed to Vercel.
-// To restore, move this file back into api/.
-
-module.exports = function handler(req, res) {
-  res.status(410).json({ error: 'Endpoint disabled in this deployment. Restore file from api_disabled/ if needed.' });
-};
