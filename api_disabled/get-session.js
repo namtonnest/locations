dd0b7c64a24e4a340bdafa5da239e236e07ed030
Nam Ton <namtonnest@gmail.com>
2025-09-07 18:15:03 -0400

    chore: disable unused api routes to reduce Vercel serverless function count

---
+++ api_disabled/get-session.js
@@ -0,0 +1,7 @@
+// MOVED: api/get-session.js
+// Disabled to reduce number of serverless functions deployed to Vercel.
+// To restore, move this file back into api/.
+
+module.exports = function handler(req, res) {
+  res.status(410).json({ error: 'Endpoint disabled in this deployment. Restore file from api_disabled/ if needed.' });
+};
